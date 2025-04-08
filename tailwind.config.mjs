8575285b15ccea8c78a5dc110a9db48aff47f1cb
Nicholas Warren <approximatelyhuman@proton.me>
2025-04-08 11:18:37 -0400

    added tailwind and inter font

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+// tailwind.config.mjs
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
